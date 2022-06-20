2bc79785ec975d2e242c34e4b08af485150d5ea5
TranTam <tamvaa1306@gmail.com>
2022-06-20 11:45:49 +0700

    create core component (item)

---
+++ src/components/ItemsComp.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="col-sm-6 col-md-4 col-lg-3">
+    <div class="card">
+      <div class="card-body item">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.card-body {
+  font-family: "Poppins", sans-serif;
+  font-size: 24px;
+  color: #6e6e6e;
+}
+.item {
+  cursor: pointer;
+  background-color: #e2e2e2;
+}
+</style>
---
+++ src/components/ItemsLayout.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <span>{{ items }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["items"],
+};
+</script>
