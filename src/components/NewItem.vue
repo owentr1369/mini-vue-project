5978f60de3dd81dc60e83e58e50a458e4b0a1e88
TranTam <tamvaa1306@gmail.com>
2022-06-20 13:15:19 +0700

    create newItem form

---
+++ src/components/NewItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="row">
+    <form action="">
+      <div class="col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
+        <label for="">Say...</label>
+        <textarea
+          class="form-group"
+          name=""
+          id=""
+          cols="30"
+          rows="10"
+          v-model="item"
+        ></textarea>
+      </div>
+      <div class="col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
+        <button class="btn btn-success" @click.prevent="createNew"></button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      item: "",
+    };
+  },
+};
+</script>
+
+<style>
+</style>
