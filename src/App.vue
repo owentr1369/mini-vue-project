028fd58c891a4f14bb96b773cd0400666de8bc11
TranTam <tamvaa1306@gmail.com>
2022-06-20 11:55:42 +0700

    render props items

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ export default {
   components: { appItemsLayout: ItemsLayout },
   data: function () {
     return {
-      items: ["Good morning"],
+      items: ["Good morning", "Hello world", "Hola"],
       maxItems: 10,
     };
   },
@@ -25,6 +25,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 80px;
 }
 </style>
